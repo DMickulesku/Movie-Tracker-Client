92bafb7f971ae12716e34af0216c5b800b254e02
DMickulesku <danmickulesku@gmail.com>
2017-07-27 14:41:56 -0600

    add to favorites button

---
+++ app.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+  getMovies();
+  $('.submitbtn').click(addFavorite)
+  $('.removebtn').click(removeFavorite)
+})
+var baseUrl = 'https://secret-taiga-92136.herokuapp.com/movies/'
+
+function getMovies() {
+  $.get('https://secret-taiga-92136.herokuapp.com/movies')
+    .then(function(data) {
+      console.log(data);
+
+      for (var i = 0; i < data.length; i++) {
+        $('#userInput').append(
+          `<option value="${data[i].id}" id="${data[i].id}">${data[i].title}</option>`
+        )
+      }
+    })
+}
+
+function addFavorite() {
+  var id = $('#userInput option:selected').val()
+  $.get(baseUrl + id)
+    .then(function(movie) {
+      $('#favInput').append(`<option value="${movie[0].id}" id="${movie[0].id}">${movie[0].title}</option>`)
+    })
+  console.log(id);
+}
+
+function removeFavorite() {
+  var id = $('#favInput option:selected').val()
+  $.get(baseUrl + id)
+    .then(function(movie) {
+      $('#favInput').remove(`<option value="${movie[0].id}" id="${movie[0].id}">${movie[0].title}</option>`)
+    })
+  console.log(id);
+}
